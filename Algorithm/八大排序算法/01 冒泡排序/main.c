b60fc3a606e50fdbb5d5739b4df626a2d9ea26bc
zhouyu <1512450002@qq.com>
2018-12-14 14:43:15 +0800

    冒泡排序

--- Algorithm/八大排序算法/01 冒泡排序/main.c
+++ Algorithm/八大排序算法/01 冒泡排序/main.c
@@ -4,12 +4,37 @@
 //
 //  Created by zhouyu on 2018/12/14.
 //  Copyright © 2018 zhouyu. All rights reserved.
-//
+/*
+ 冒泡排序算法的原理如下：
+ 1.  比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+ 2. 对每一对相邻元素做同样的工作，从开始第一对到结尾的最后一对。在这一点，最后的元素应该会是最大的数。
+ 3. 针对所有的元素重复以上的步骤，除了最后一个。
+ 4. 持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较
+ */
 
 #include <stdio.h>
 
+void bubbleSort(int arr[], int length) {
+    for (int i = 0; i < length - 1; i++) {//比较n趟, 每次确定一个元素
+        for (int j = 0; j < length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int nums[10] = {4, 5, 2, 10, 7, 1, 8, 3, 6, 9};
+    int length = sizeof(nums) / sizeof(nums[0]);
+    bubbleSort(nums, length);
+    
+    for (int i = 0; i < length; i++) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
     return 0;
 }
